7c7d576288e1d1eed6adc6987bcff4f0ae86a37a
joe <joenasr@email.com>
2025-04-08 17:08:56 +0300

    Refactor app.py for improved logging and MongoDB integration

--- app.py
+++ app.py
@@ -1,26 +1,64 @@
 import os
-from langchain_core.messages import HumanMessage, AIMessage, SystemMessage, AIMessageChunk
+from langchain_core.messages import HumanMessage, AIMessage, AIMessageChunk, trim_messages
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langgraph.checkpoint.memory import MemorySaver
+from langgraph.checkpoint.mongodb import AsyncMongoDBSaver
 from langgraph.prebuilt import create_react_agent
 from dotenv import load_dotenv
 from tools import get_weather, get_now_playing_movies, search_web, get_hub_stats
 from retriever import load_guest_dataset
+from pymongo import AsyncMongoClient
 import gradio as gr
 import logging
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
 load_dotenv()
 
-llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0.4, streaming=True)
-memory = MemorySaver()
+# Configure logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+file_handler = logging.FileHandler(filename='agent.log', encoding='utf-8', mode='a')
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(file_handler)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+logger.addHandler(console_handler)
+
+# Initialize model and tools
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0.2, streaming=True)
 guest_tool = load_guest_dataset()
 tools = [get_weather, get_now_playing_movies, search_web, get_hub_stats, guest_tool]
 
-agent = create_react_agent(llm, tools, checkpointer=memory)
-config = {"configurable": {"thread_id": "my-langchain-agent"}}
+# Persistence layer
+def setup_persistence():
+    try:
+        host = os.getenv('MONGODB_URI')
+        if not host:
+            raise ValueError("MONGODB_URI environment variable not set")
+        mongodb_client = AsyncMongoClient(host)
+        checkpointer = AsyncMongoDBSaver(mongodb_client)
+    except Exception as e:
+        logger.error(f"Error connecting to MongoDB: {e}")
+        logger.info("Falling back to MemorySaver")
+        checkpointer = MemorySaver()
+    return checkpointer
+checkpointer = MemorySaver() #setup_persistence()
+
+def pre_model_hook(state):
+    # Keep the last 5 messages and remove the oldest ones
+    trimmed_messages = trim_messages(state["messages"],
+                                    token_counter=len,
+                                    strategy="last",
+                                    max_tokens=5,
+                                    start_on="human",
+                                    end_on=("human", "tool"),
+                                    include_system=True)
+    return {"llm_input_messages": trimmed_messages}
+
+agent = create_react_agent(llm, tools, checkpointer=checkpointer, pre_model_hook=pre_model_hook)
+config = {"configurable": {"user_id": "user-xxx", "thread_id": "my-langchain-agent"}}
 
 async def chat(query: str, hist: list):
     try:
@@ -29,6 +67,7 @@ async def chat(query: str, hist: list):
             yield "", hist
         else:
             hist.append(gr.ChatMessage(role="user", content=query))
+            logger.info(f"User Message: {query}")
             yield "", hist
             buffer = ""
             async for chunk, _ in agent.astream({"messages": [HumanMessage(content=query)]}, config, stream_mode="messages"):
@@ -37,6 +76,7 @@ async def chat(query: str, hist: list):
                         for tool_call in chunk.tool_calls:
                             tool_name = tool_call.get('name', 'Unknown Tool')
                             tool_args = tool_call.get('args', 'no args')
+                            logger.info(f"Invoking tool: {tool_name} with arguments: {tool_args}")
                             
                             # Format the tool call and arguments
                             hist.append(gr.ChatMessage(role="assistant",
@@ -48,6 +88,7 @@ async def chat(query: str, hist: list):
                         msg = gr.ChatMessage(role="assistant", content=buffer)
                         yield "", hist + [msg]
             hist.append(gr.ChatMessage(role="assistant", content=buffer))
+            logger.info(f"AI response from assistant: {buffer[:50]}...")
             yield "", hist
     except Exception as e:
         logger.error(f"Error in chat function: {e}")
